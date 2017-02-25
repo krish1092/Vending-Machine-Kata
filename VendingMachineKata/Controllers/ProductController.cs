2473f9fbf2a0940ba9870181de31ea97470fcc97
krish1092 <krish1610@gmail.com>
2017-02-25 16:58:01 -0600

    Adding the list of products to the UI.

--- VendingMachineKata/Controllers/ProductController.cs
+++ VendingMachineKata/Controllers/ProductController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VendingMachineKata.Models;
 
 namespace VendingMachineKata.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: Product
-        public ActionResult Index()
+        // GET: Product/GetProducts
+        public ActionResult GetProducts()
         {
-            return View();
+            Product cola = new Product("Cola", 1.00, 10);
+            Product chips = new Product("Chips", 0.50, 10);
+            Product candy = new Product("Candy", 0.65, 10);
+
+            List<Product> ListOfProducts = new List<Product>();
+            ListOfProducts.Add(cola);
+            ListOfProducts.Add(chips);
+            ListOfProducts.Add(candy);
+
+            return View(ListOfProducts);
         }
     }
 }
--- VendingMachineKata/Models/Product.cs
+++ VendingMachineKata/Models/Product.cs
@@ -7,8 +7,16 @@ namespace VendingMachineKata.Models
 {
 	public class Product
 	{
-        private string ProductName { get; set; }
-        private int ProductPrice { get; set; }
-        private int ProductCount { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public int ProductCount { get; set; }
+
+
+        public Product (string name, double price, int count)
+        {
+            this.ProductName = name;
+            this.ProductPrice = price;
+            this.ProductCount = count;
+        }
     }
 }
