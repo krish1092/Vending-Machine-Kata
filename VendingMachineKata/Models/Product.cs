2e069ed1179e816dff1a19ad3246ab3972e53603
krish1092 <krish1610@gmail.com>
2017-02-25 13:52:05 -0600

    Added Product Class

---
+++ VendingMachineKata/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VendingMachineKata.Models
+{
+	public class Product
+	{
+        private string ProductName { get; set; }
+        private int ProductPrice { get; set; }
+        private int ProductCount { get; set; }
+    }
+}
