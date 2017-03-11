dcba88df8e469bae44c9db70044fe97fa807d0ed
krish <krish1610@gmail.com>
2017-03-10 20:26:39 -0600

    Changes in VendingService Class

--- VendingMachineKata/Service/VendingService.cs
+++ VendingMachineKata/Service/VendingService.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using VendingMachineKata.Models;
 
 namespace VendingMachineKata.Service
 {
-    public class VendingService
+    public static class VendingService
     {
 
+        //For getting the list of products
+        private static List<Product> ListOfProducts = new List<Product>(){
+            new Product("Cola", 1.00, 10),
+            new Product("Chips", 0.50, 10),
+            new Product("Candy", 0.65, 10)
+        };
+
         //For Tendering change as required
-        private static Dictionary<double, int> coinValues = new Dictionary<double, int> { { 0.25 , 10}, { 0.10 , 10}, { 0.05 , 10} };
+        private static Dictionary<double, int> coinValues = new Dictionary<double, int> {
+            { 0.25 , 10},
+            { 0.10 , 10},
+            { 0.05 , 10}
+        };
         
 
         //The algorithm goes from quarters to nickels. Tries to give as many quarters,dimes, nickels(in this order) as possible
-        public double TenderChange(double RemainingAmount){
+        public static double TenderChange(double RemainingAmount){
 
             foreach (double coinValue in coinValues.Keys)
             {
@@ -44,7 +56,7 @@ namespace VendingMachineKata.Service
 
 
 
-        public void AddToExistingChange(double[] coinsInserted)
+        public static void AddToExistingChange(double[] coinsInserted)
         {
             //The customer has inserted a few coins as well. Add them to the pool of coins that already exist and then tender change
             foreach (double coinValue in coinsInserted)
